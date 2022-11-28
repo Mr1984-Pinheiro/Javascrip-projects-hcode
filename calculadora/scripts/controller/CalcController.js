b6d215f714b1c8fcd77785da65b4b6d0edffc41b
Eduardo <cadutapinheiro@gmail.com>
2022-11-28 03:06:08 -0300

    manipulating the DOM

--- calculadora/scripts/controller/CalcController.js
+++ calculadora/scripts/controller/CalcController.js
@@ -4,6 +4,17 @@ class CalcController {
 
         this._displayCalc = "0"; //underline_refere-se a atributo privado
         this._currentDate;
+        this.initialize();
+    }
+
+    initialize(){
+       let displayCalcEl = document.querySelector("#display");
+       let dateEl = document.querySelector("#data");
+       let timeEl = document.querySelector("#hora");
+
+       displayCalcEl.innerHTML = "5246";
+       dateEl.innerHTML = "01/05/1984";
+       timeEl.innerHTML = "00:00";
     }
 
     get displayCalc(){
@@ -20,7 +31,7 @@ class CalcController {
         return this._currentDate;
     }
 
-    set currentDate(data){
-        this._currentDate = data;
+    set currentDate(valor){
+        this._currentDate = valor;
     }
 }
